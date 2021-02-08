d6b0a5d7aab39f4590dd586309089ccb4e02d9ba
Beto Floripa <sacplus.chat@gmail.com>
2021-02-08 09:20:59 -0300

    Config Host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,6 +65,6 @@ Rails.application.configure do
 
   # Devise setup
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.hosts << app.sacplus.com.br
+  config.hosts << /application\.local/
   # config.hosts << /[a-z0-9]+\.ngrok\.io/
 end
