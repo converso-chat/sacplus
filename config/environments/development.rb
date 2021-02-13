04329306fc9b3e3302ac29c7f5b993700ff662cd
Beto Floripa <sacplus.chat@gmail.com>
2021-02-13 17:09:24 -0300

    Updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,8 +63,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.force_ssl = true
-
   # Devise setup
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.hosts << "www.sacplus.com.br"
